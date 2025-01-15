f3a1a15b7211287b9e8d3c6de5a8d0470d17215b
Wagner <wagnersanches5@hotmail.com>
2025-01-14 21:40:25 -0300

    Add JaCoCo

---
+++ src/test/java/br/com/fiap/microservice_payment/controller/PaymentControllertTest.java
@@ -0,0 +1,15 @@
+package br.com.fiap.microservice_payment.controller;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PaymentControllertTest {
+
+    @Test
+    public void test() {
+        int i = 1;
+        Assertions.assertEquals(1, i);
+    }
+
+}
