6ae2de8c207355ed0b60000aa571856c94a5983b
Edson <c1297934@interno.bb.com.br>
2025-02-04 17:54:13 -0300

    ajuste conexão

--- src/main/java/br/com/fiap/microservice_payment/config/DynamoDBConfig.java
+++ src/main/java/br/com/fiap/microservice_payment/config/DynamoDBConfig.java
@@ -2,8 +2,11 @@ package br.com.fiap.microservice_payment.config;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+
 import org.apache.commons.lang.StringUtils;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,29 +17,32 @@ import org.springframework.context.annotation.Configuration;
 @EnableDynamoDBRepositories(basePackages = "br.com.fiap.microservice_payment.repository")
 public class DynamoDBConfig {
 
-    @Value("${amazon.dynamodb.endpoint}")
-    private String amazonDynamoDBEndpoint;
+    // @Value("${amazon.dynamodb.endpoint}")
+    // private String amazonDynamoDBEndpoint;
 
-    @Value("${amazon.aws.accesskey}")
-    private String amazonAWSAccessKey;
+    // @Value("${amazon.aws.accesskey}")
+    // private String amazonAWSAccessKey;
 
-    @Value("${amazon.aws.secretkey}")
-    private String amazonAWSSecretKey;
+    // @Value("${amazon.aws.secretkey}")
+    // private String amazonAWSSecretKey;
+
+    private String awsRegion = "us-east-1";
 
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
-        AmazonDynamoDB amazonDynamoDB
-                = new AmazonDynamoDBClient(amazonAWSCredentials());
+        AmazonDynamoDBClientBuilder builder = AmazonDynamoDBClientBuilder
+            .standard()
+            .withRegion(awsRegion)
+            .withCredentials(DefaultAWSCredentialsProviderChain.getInstance());
 
-        if (!StringUtils.isEmpty(amazonDynamoDBEndpoint))
-            amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
+        // if (dynamoDBEndpoint != null && !dynamoDBEndpoint.isEmpty()) {
+        //     builder.withEndpointConfiguration(
+        //             new AwsClientBuilder.EndpointConfiguration(dynamoDBEndpoint, awsRegion));
+        // } else {
+        //     builder.withRegion(awsRegion)
+        //     .withCredentials(DefaultAWSCredentialsProviderChain.getInstance());
+        // }
 
-        return amazonDynamoDB;
-    }
-
-    @Bean
-    public AWSCredentials amazonAWSCredentials() {
-        return new BasicAWSCredentials(
-                amazonAWSAccessKey, amazonAWSSecretKey);
+        return builder.build();
     }
 }
